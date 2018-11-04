90c70226c76a4f09374f46ffe7329996863f85fc
Szwajowy <krzycza96@gmail.com>
2018-11-04 20:19:05 +0100

    Dodany profil i inne drobne zmiany

---
+++ src/Controller/ProfileCtrl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileCtrl extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile_index")
+     */
+    public function index()
+    {
+        return $this->render('profile/index.html.twig');
+    }
+}
